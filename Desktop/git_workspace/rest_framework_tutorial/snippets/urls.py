d18668ba4c048ec795a359bc83201c6231f898e3
audgks5551 <audgks5551@daum.net>
2022-01-15 12:48:26 +0900

    rest_framework를 이용하여 view 작성

--- Desktop/git_workspace/rest_framework_tutorial/snippets/urls.py
+++ Desktop/git_workspace/rest_framework_tutorial/snippets/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+#
+from rest_framework.urlpatterns import format_suffix_patterns
+#
 from snippets import views
 
 app_name = "snippets"
@@ -7,3 +10,5 @@ urlpatterns = [
     path("snippets/", views.snippet_list, name="list"),
     path("snippets/<int:pk>/", views.snippet_detail, name="detail"),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
